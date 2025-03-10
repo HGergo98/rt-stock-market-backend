ef565d031c704dc1eb7f05f9f9526e328ca5015f
IHGergo <gergo.hegyi@ikontent.hu>
2025-03-10 13:48:27 +0100

    feat: add TODOs for the remaining features

--- backend/main.go
+++ backend/main.go
@@ -11,4 +11,16 @@ func main() {
 
 	// Database connection
 	db := db.InitDB(envConfig, db.DBMigrator)
+
+	// TODO: Connect to Finhubb Websockets
+
+	// TODO: Handle incoming messages
+
+	// TODO: Broadcast messages to all connected clients
+
+	// TODO: Endpoint for connect to the websocket
+	// TODO: Endpoint for fetching all the past candles for all the symbols
+	// TODO: Endpoint for fetching all the past candles for a symbol
+
+	// TODO: Serve the endpoints
 }
